a2e94cb0dc41da3262c4da84db6acf5ba131a76b
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-06-20 11:37:06 +0300

    feat: App barebones

---
+++ push-app/app/(tabs)/history.tsx
@@ -0,0 +1,9 @@
+import {View, Text, StyleSheet} from 'react-native';
+import { Heading } from 'tamagui';
+export default function History(){
+    return (
+        <View>
+
+        </View>
+    );
+}
